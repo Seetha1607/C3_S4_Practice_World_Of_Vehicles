ec52796aad6c90f0a909937bd4a59a091e6a6828
Seetha <seethalakshmi1607@gmail.com>
2022-08-29 20:00:46 +0530

    created engine as object class

--- src/main/java/com/engine/EngineImpl.java
+++ src/main/java/com/engine/EngineImpl.java
@@ -3,6 +3,5 @@ package com.engine;
 public class EngineImpl {
     public static void main(String[] args) {
         Engine engine = new Engine();
-        engine.setModelNumber("AB-3478SN");
     }
 }
