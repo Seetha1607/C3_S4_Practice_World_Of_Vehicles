f796041626f1a40b6b8fcb6c3703ef46d66c82a5
Seetha <seethalakshmi1607@gmail.com>
2022-08-29 20:13:01 +0530

    initialized the values of CNGEngine class using setter and printed the statement using getter

--- src/main/java/com/engine/EngineImpl.java
+++ src/main/java/com/engine/EngineImpl.java
@@ -23,6 +23,7 @@ public class EngineImpl {
         System.out.println("The fuel type of Diesel Engine : " + diesel.getFuelType());
 
         CNGEngine cng = new CNGEngine();
-
+        cng.setFuelType("CNG");
+        System.out.println("The fuel type of CNG Engine : " + cng.getFuelType());
     }
 }
