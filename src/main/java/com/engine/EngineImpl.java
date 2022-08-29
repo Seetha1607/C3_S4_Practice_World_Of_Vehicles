ef0b83cfe417dddaf889c1632db6400f24b9f632
Seetha <seethalakshmi1607@gmail.com>
2022-08-29 20:01:33 +0530

    using setter, initialized the values of fields of engine class

--- src/main/java/com/engine/EngineImpl.java
+++ src/main/java/com/engine/EngineImpl.java
@@ -3,5 +3,10 @@ package com.engine;
 public class EngineImpl {
     public static void main(String[] args) {
         Engine engine = new Engine();
+        engine.setModelNumber("AB-3478SN");
+        engine.setDisplacement(1497);
+        engine.setMaxHorsePower(117);
+        engine.setRevolutionsPerMinute(6600);
+        engine.setNumberOfCylinders(4);
     }
 }
